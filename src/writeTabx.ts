0848582472e66ef930105a117505df06b3ac43ef
Chirod Summerset <kyoinbeijing@163.com>
2025-02-05 14:45:14 +0800

    优化了buildTabxChunk函数（现在以预估文件大小为分区节点而不是条目数）

--- src/writeTabx.ts
+++ src/writeTabx.ts
@@ -2,13 +2,63 @@ import { UnifiedBalloon } from "./types/csvTypes";
 import { TabxType } from "./types/tabxTypes";
 import * as fs from 'fs';
 import chalk from 'chalk';
-
+// verboseChrSize是测试时的大致估算（即标识符和缩进等字符的大致占位）。可以通过实际输出情况进行调整。
+const VERBOSE_CHR_SIZE = 300;
+// TABX_INDENT_SIZE是tabx文件的缩进大小。会极大影响VERBOSE_CHR_SIZE的准确性。   
+const TABX_INDENT_SIZE = 2;
 type UnifiedBalloonTabxColumns = [string, string, number, string, string, string, string, string, string, string];
-const buildTabxChunk = (data: UnifiedBalloon[], chunkSize = 2500): TabxType<UnifiedBalloonTabxColumns>[] => {
+const buildTabxChunkBySize = (data: UnifiedBalloon[], chunkSize = 1800000): TabxType<UnifiedBalloonTabxColumns>[] => {
+    const getEntrySize = (entry: UnifiedBalloon) => {
+        return (entry.text_EN?.length ?? 0) + (entry.text_JA?.length ?? 0) + (entry.text_CHS?.length ?? 0) + (entry.text_FR?.length ?? 0) + (entry.text_DE?.length ?? 0) + (entry.text_KO?.length ?? 0) + VERBOSE_CHR_SIZE;
+    }
     const chunks: TabxType<UnifiedBalloonTabxColumns>[] = [];
-    console.log(`共有${data.length}条数据。按照当前的chunkSize=${chunkSize}，将分为${Math.ceil(data.length / chunkSize)}个文件。`);
-    for (let i = 0; i < data.length; i += chunkSize) {
-        const chunk = data.slice(i, i + chunkSize);
+    let currentChunk: UnifiedBalloon[] = [];
+    let currentChunkSize = 0;
+    for (const entry of data) {
+        const entrySize = getEntrySize(entry);
+        if (currentChunkSize + entrySize > chunkSize) {
+
+            console.log(`当前chunk的大小为${currentChunkSize}，将写入一个新的chunk。`);
+            chunks.push({
+                description: {
+                    zh: '统一的NPC对话气泡和喊话数据'
+                },
+                schema: {
+                    fields: [
+                        { name: 'data_type', type: 'string', title: { en: 'data_type', zh: 'data_type' } },
+                        { name: 'uid', type: 'string', title: { en: 'uid', zh: 'uid' } },
+                        { name: 'id', type: 'number', title: { en: 'id', zh: 'id' } },
+                        { name: 'source', type: 'string', title: { en: 'source', zh: 'source' } },
+                        { name: 'text_KO', type: 'string', title: { en: 'text_KO', zh: 'text_KO' } },
+                        { name: 'text_EN', type: 'string', title: { en: 'text_EN', zh: 'text_EN' } },
+                        { name: 'text_JA', type: 'string', title: { en: 'text_JA', zh: 'text_JA' } },
+                        { name: 'text_CHS', type: 'string', title: { en: 'text_CHS', zh: 'text_CHS' } },
+                        { name: 'text_FR', type: 'string', title: { en: 'text_FR', zh: 'text_FR' } },
+                        { name: 'text_DE', type: 'string', title: { en: 'text_DE', zh: 'text_DE' } }
+                    ]
+                },
+                data: currentChunk.map(item => {
+                    return [
+                        item.data_type,
+                        item.uid,
+                        item.id,
+                        item.source,
+                        item.text_KO ?? '',
+                        item.text_EN ?? '',
+                        item.text_JA ?? '',
+                        item.text_CHS ?? '',
+                        item.text_FR ?? '',
+                        item.text_DE ?? ''
+                    ]
+                })
+            });
+            currentChunk = [];
+            currentChunkSize = 0;
+        }
+        currentChunk.push(entry);
+        currentChunkSize += entrySize;
+    }
+    if (currentChunk.length > 0) {
         chunks.push({
             description: {
                 zh: '统一的NPC对话气泡和喊话数据'
@@ -27,7 +77,7 @@ const buildTabxChunk = (data: UnifiedBalloon[], chunkSize = 2500): TabxType<Unif
                     { name: 'text_DE', type: 'string', title: { en: 'text_DE', zh: 'text_DE' } }
                 ]
             },
-            data: chunk.map(item => {
+            data: currentChunk.map(item => {
                 return [
                     item.data_type,
                     item.uid,
@@ -41,19 +91,17 @@ const buildTabxChunk = (data: UnifiedBalloon[], chunkSize = 2500): TabxType<Unif
                     item.text_DE ?? ''
                 ]
             })
-        })
+        });
     }
     return chunks;
 }
 
 (async () => {
-    // read ./output/unified_npc_balloon_allentries.json. if not exists, console.error and return.
     const filePath = './output/unified_npc_balloon_allentries.json';
     if (!fs.existsSync(filePath)) {
         console.error('File not exists');
         return;
     }
-    // delete all existed jsonfiles starts with 'unified_npc_balloon_chunk' in ./output
     console.log(chalk.yellow('删除已存在的分段文件...'));
     fs.readdirSync('./output').forEach(file => {
         if (file.startsWith('unified_npc_balloon_chunk') && file.endsWith('.json')) {
@@ -63,12 +111,12 @@ const buildTabxChunk = (data: UnifiedBalloon[], chunkSize = 2500): TabxType<Unif
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const data: UnifiedBalloon[] = JSON.parse(fileContent);
 
-    const chunks = buildTabxChunk(data);
+    const chunks = buildTabxChunkBySize(data);
 
     for (const [index, chunk] of chunks.entries()) {
-        fs.writeFileSync(`./output/unified_npc_balloon_chunk_${index}.json`, JSON.stringify(chunk, null, 4));
+        fs.writeFileSync(`./output/unified_npc_balloon_chunk_${index}.json`, JSON.stringify(chunk, null, TABX_INDENT_SIZE));
         // get the file size
         const stats = fs.statSync(`./output/unified_npc_balloon_chunk_${index}.json`);
-        console.log(`./output/unified_npc_balloon_chunk_${index}.json 写入成功。文件大小为${stats.size}字节。`);
+        console.log(`./output/unified_npc_balloon_chunk_${index}.json 写入成功。文件大小为${stats.size}字节，条目数${chunk.data.length}。`);
     }
 })()
